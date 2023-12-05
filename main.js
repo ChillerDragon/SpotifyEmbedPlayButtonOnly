3dfd41281df5f7f3e5897d3a30d3412e0a23d441
ChillerDragon <ChillerDragon@gmail.com>
2023-12-05 11:32:20 +0100

    Minor improvements

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 window.onSpotifyIframeApiReady = (IFrameAPI) => {
-  let element = document.getElementById('embed-iframe');
-  let options = {
+  const element = document.getElementById('embed-iframe');
+  const options = {
       uri: 'spotify:episode:7makk4oTQel546B0PZlDM5'
   };
-  let callback = (EmbedController) => {
-    const iframes = document.querySelectorAll("iframe")
+  const callback = (EmbedController) => {
+    const iframes = document.querySelectorAll("iframe");
     iframes.forEach((iframe) => {
+      iframe.style.opacity = 0;
+      iframe.style.display = "none";
       iframe.width = 1;
       iframe.height = 1;
     })
     const playBtn = document.querySelector('.play-btn');
-    console.log(playBtn);
     playBtn.addEventListener('click', (e) => {
       EmbedController.play();
     })
